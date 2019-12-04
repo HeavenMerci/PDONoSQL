a93e68624d17b4f3a790e3c12db70946c8de511c
Dick Claudel <dick_claudel@yahoo.fr>
2019-12-04 09:07:12 +0100

    adds the possibility to specify the test to execute in the file (pdo_nosql.test.php) adds the file 'test_db.sql' on which depends the test file

--- pdo_nosql.test.php
+++ pdo_nosql.test.php
@@ -2,100 +2,139 @@
 
 include_once 'pdonosql/utils/autoload_class.php';
 
-$connection = new PDO('mysql:host=localhost;dbname=test', 'root', 'root');
+
+const HOST=  'localhost';
+const USRNAME = 'root';
+const PASSWD = 'root';
+
+/* correstpond to the database in the file 'test_db.sql */
+const DBNAME = 'test';
+
+
+
+$connection = new PDO('mysql:host='.HOST.';dbname='.DBNAME, USRNAME, PASSWD);
 $nosql = new pdonosql\PDONoSQL( $connection );
 
 
+// ---------------------------------------------------------------------------------
+
+
+/*  to set the test to execute
+- CREATE:               1
+- READ:                 2
+- UPDATE:               3
+- DELETE:               4
+- GET TABLES LIST:      5
+- GET TABLE FIELDS:     6
+----------------------------- */
+const TEST_QUERY_ID = 6;
 
 
 /* CREATE TEST
 ----------------------------- */
-// $result = $nosql
-//     ->in( 'employee' )
-//     ->if( new pdonosql\check\AndBag(
-//         new pdonosql\check\Greater('salary', 49999),
-//         new pdonosql\check\Equal('branch_id', 4) ))
-//     ->create([
-//         'first_name' => 'John',
-//         'last_name' => 'Doe',
-//         'birth_date' => '1994-02-27',
-//         'sex' => 'M',
-//         'salary' => 50000,
-//         'branch_id' => 4
-//     ]);
+if( TEST_QUERY_ID == 1 ){
+    $result = $nosql
+        ->in( 'employee' )
+        ->if( new pdonosql\check\AndBag(
+            new pdonosql\check\Greater('salary', 49999),
+            new pdonosql\check\Equal('branch_id', 4) ))
+        ->create([
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'birth_date' => '1994-02-27',
+            'sex' => 'M',
+            'salary' => 50000,
+            'branch_id' => 4
+        ]);
+}
 
-
+else
 
 /* READ TEST
 ----------------------------- */
-// $result = "number of employees per branch = ".pdonosql\utils\Utils::toJSON(
-
-// $nosql->clear()
-//     ->in( 'employee' )
-//     ->andIn( 'branch' )
-//     ->groupBy( 'branch_id' )
-//     //->if( new pdonosql\check\Greater('emp_count', 2), false )
-//     ->read( 'branch_name', pdonosql\utils\Utils::numberOf('emp_id', 'emp_count') )
-
-// , true);
-
-// $result .= "\n\nsome branch managers informations = ".pdonosql\utils\Utils::toJSON(
-
-//     $nosql->clear()
-//         ->in( 'employee' )
-//         ->andIn( 'branch', 'mgr_id', 'emp_id' )
-//         ->orderBy( pdonosql\utils\Utils::descending('salary') )
-//         ->if( new pdonosql\check\OrBag(
-//             new pdonosql\check\Equal('last_name', 'Mercy'),
-//             new pdonosql\check\Equal('last_name', 'Scott') ) )
-//         ->read( pdonosql\utils\Utils::concat(['first_name', 'last_name'], "mgr_name"),
-//             'salary', 'branch_name' )
-
-// , true);
+if( TEST_QUERY_ID == 2 ){
+    $result = "number of employees per branch = ".pdonosql\utils\Utils::toJSON(
+        $nosql->clear()
+            ->in( 'employee' )
+            ->andIn( 'branch' )
+            ->groupBy( 'branch_id' )
+            //->if( new pdonosql\check\Greater('emp_count', 2), false )
+            ->read( 'branch_name', pdonosql\utils\Utils::numberOf('emp_id', 'emp_count') )
+    , true);
 
 
-// $result .= "\n\nlist of employees = ".pdonosql\utils\Utils::toJSON(
+    $result .= "\n\nsome branch managers informations = ".pdonosql\utils\Utils::toJSON(
+        $nosql->clear()
+            ->in( 'employee' )
+            ->andIn( 'branch', 'mgr_id', 'emp_id' )
+            ->orderBy( pdonosql\utils\Utils::descending('salary') )
+            ->if( new pdonosql\check\OrBag(
+                new pdonosql\check\Equal('last_name', 'Mercy'),
+                new pdonosql\check\Equal('last_name', 'Scott') ) )
+            ->read( pdonosql\utils\Utils::concat(['first_name', 'last_name'], "mgr_name"),
+                'salary', 'branch_name' )
+    , true);
 
-//     $nosql->clear()
-//         ->in( 'employee' )
-//         ->orderBy( 'birth_date' )
-//         ->takeOnly( 5 )
-//         ->read( 'emp_id', 'first_name', 'last_name', 'birth_date' )
 
-// , true);
+    $result .= "\n\nlist of employees = ".pdonosql\utils\Utils::toJSON(
+        $nosql->clear()
+            ->in( 'employee' )
+            ->orderBy( 'birth_date' )
+            ->takeOnly( 5 )
+            ->read( 'emp_id', 'first_name', 'last_name', 'birth_date' )
+    , true);
+}
 
-
+else
 
 /* UPDATE TEST
 ----------------------------- */
-// $result = $nosql
-//     ->in( 'employee' )
-//     ->if( new pdonosql\check\Equal('emp_id', 110) )
-//     ->update( ['super_id' => 109], new pdonosql\check\NotEqual('super_id', 110) );
+if( TEST_QUERY_ID == 3 ){
+    $result = $nosql
+        ->in( 'employee' )
+        ->if( new pdonosql\check\Equal('emp_id', 110) )
+        ->update( ['super_id' => 109], new pdonosql\check\NotEqual('super_id', 110) );
+}
 
-
+else
 
 /* DELETE TEST
 ----------------------------- */
-// $nosql = new pdonosql\PDONoSQL(
-//     new PDO('mysql:host=localhost;dbname=sftest', 'root', 'root') );
+if( TEST_QUERY_ID == 4 ){
+    $nosql = new pdonosql\PDONoSQL(
+        new PDO('mysql:host=localhost;dbname=sftest', 'root', 'root') );
 
-// $result = $nosql
-//     ->in( 'todo' )
-//     ->if( new pdonosql\check\Equal('id', 7) )
-//     ->delete();
+    $result = $nosql
+        ->in( 'todo' )
+        ->if( new pdonosql\check\Equal('id', 7) )
+        ->delete();
+}
+
+else
+
+/* GET TABLES TEST
+----------------------------- */
+if( TEST_QUERY_ID == 5 ){
+    $result = pdonosql\utils\Utils::toJSON(
+        $nosql->clear()
+            ->getTables()
+    , true);
+}
+
+else
+
+/* GET TABLE FIELDS TEST
+----------------------------- */
+if( TEST_QUERY_ID == 6 ){
+    $result = pdonosql\utils\Utils::toJSON(
+        $nosql->clear()
+            ->in("branch")
+            ->getFields()
+    , true);
+}
 
 
 
-
-
-$result = pdonosql\utils\Utils::toJSON(
-
-$nosql->clear()
-    ->getTables()
-
-);
-
-
-if( $nosql->isOK() ) echo $result;
-else echo 'Error: '.$nosql->getException()->getMessage();
+if( $nosql->isOK()){
+    if( isset($result) ) echo $result;
+}else echo 'Error: '.$nosql->getException()->getMessage();
